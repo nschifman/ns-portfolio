b04e3ad6a21573c32ba85c141c87cc272db68d64
Test User <test@example.com>
2025-07-25 22:55:37 -0400

    Fix blank page issue and simplify browser title

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' && process.env.GITHUB_PAGES ? '/ns-portfolio/' : '/',
+  base: mode === 'production' ? '/ns-portfolio/' : '/',
   server: {
     host: "::",
     port: 8080,
